4eab2dbd9192bad297127bafac48cad183c528a4
LittringerJ <jlittringer@objectbay.com>
2023-07-29 20:07:27 +0200

    update nestjs

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
-import { FileInterceptor } from '@nestjs/platform-express';
 import { WhisperService } from './services/whisper.service';
 import { ApiBody, ApiConsumes, ApiProperty } from '@nestjs/swagger';
-
+import { FileInterceptor } from '@nestjs/platform-express';
 
 export class FileUploadDto {
   @ApiProperty({ type: 'string', format: 'binary' })
@@ -12,7 +17,10 @@ export class FileUploadDto {
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService, private readonly whisperService: WhisperService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly whisperService: WhisperService,
+  ) {}
 
   @Get()
   getHello(): string {
